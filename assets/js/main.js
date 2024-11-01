12adf6741a8c97739c458c41f76226d9b1a96659
HuyHa <107118105+HaHuy34@users.noreply.github.com>
2024-11-01 13:29:47 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,42 @@
+// Animation wweb
+AOS.init();
+
+// Slider
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 1000, // 1 giây
-      disableOnInteraction: false // vẫn tiếp tục tự động chuyển slide sau khi người dùng tương tác
-    },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 1000, // 1 giây
+    disableOnInteraction: false, // vẫn tiếp tục tự động chuyển slide sau khi người dùng tương tác
+  },
+});
+
+var swiper = new Swiper(".leverSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 100000, // 1 giây
+    disableOnInteraction: false, // vẫn tiếp tục tự động chuyển slide sau khi người dùng tương tác
+  },
 });
 
 // JavaScript để theo dõi vị trí cuộn
 window.addEventListener("scroll", function () {
-    var headerBottom = document.querySelector(".header-botttom");
-    if (window.scrollY > 50) { // Khi cuộn quá 50px
-        headerBottom.classList.add("scrolled");
-    } else {
-        headerBottom.classList.remove("scrolled");
-    }
+  var headerBottom = document.querySelector(".header-botttom");
+  if (window.scrollY > 50) {
+    // Khi cuộn quá 50px
+    headerBottom.classList.add("scrolled");
+  } else {
+    headerBottom.classList.remove("scrolled");
+  }
 });
-
